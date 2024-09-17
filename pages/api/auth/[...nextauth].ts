74959802999400b09b90857e422fd2ab596c7f40
Tung Mai <tung.mai@asnet.com.vn>
2024-09-17 18:37:48 +0700

    Fix next auth error

--- pages/api/auth/[...nextauth].ts
+++ pages/api/auth/[...nextauth].ts
@@ -1,13 +1,10 @@
 import { NextApiHandler } from 'next';
-import NextAuth from 'next-auth';
+import NextAuth, { NextAuthOptions } from 'next-auth';
 import { PrismaAdapter } from '@next-auth/prisma-adapter';
 import GitHubProvider from 'next-auth/providers/github';
 import prisma from '../../../lib/prisma';
 
-const authHandler: NextApiHandler = (req, res) => NextAuth(req, res, options);
-export default authHandler;
-
-const options = {
+export const authOptions: NextAuthOptions = {
   providers: [
     GitHubProvider({
       clientId: process.env.GITHUB_ID,
@@ -16,4 +13,7 @@ const options = {
   ],
   adapter: PrismaAdapter(prisma),
   secret: process.env.SECRET,
-};
+}
+
+const authHandler: NextApiHandler = (req, res) => NextAuth(req, res, authOptions);
+export default authHandler;
--- pages/api/post/index.ts
+++ pages/api/post/index.ts
@@ -1,5 +1,6 @@
-import { getSession } from 'next-auth/react';
+import { getServerSession } from 'next-auth';
 import prisma from '../../../lib/prisma';
+import { authOptions } from '../auth/[...nextauth]';
 
 // POST /api/post
 // Required fields in body: title
@@ -7,8 +8,8 @@ import prisma from '../../../lib/prisma';
 export default async function handle(req, res) {
   const { title, content } = req.body;
 
-  const session = await getSession({ req });
-  console.log('SESSION', session)
+  const session = await getServerSession(req, res, authOptions)
+
   const result = await prisma.post.create({
     data: {
       title: title,
